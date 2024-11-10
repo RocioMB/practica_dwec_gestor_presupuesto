0f671fb82414c3bd916344c93d84a5314f25a6c4
Rocio Martinez Baltanas <mbrocio@gmail.com>
2024-11-11 00:20:09 +0100

    Gastos creados y añadidos

--- js/generarDatosEstaticos.js
+++ js/generarDatosEstaticos.js
@@ -5,3 +5,17 @@ import {mostrarPresupuesto, actualizarPresupuesto, CrearGasto, listarGastos, any
 actualizarPresupuesto(1500);
 
 mostrarDatoEnId("presupuesto", mostrarPresupuesto());
+
+let gasto1 = new CrearGasto("Compra carne", "23.44", "2021-10-06", "casa", "comida");
+let gasto2 = new CrearGasto("Compra fruta y verdura", "14.25", "2021-09-06", "supermercado", "comida");
+let gasto3 = new CrearGasto("Bonobús", "18.60", "2020-05-26", "transporte");
+let gasto4 = new CrearGasto("Gasolina", "60.42", "2021-10-08", "transporte", "gasolina");
+let gasto5 = new CrearGasto("Seguro hogar", "206.45", "2021-09-26", "casa", "seguros");
+let gasto6 = new CrearGasto("Seguro coche", "195.78", "2021-10-06", "transporte", "seguros");
+
+anyadirGasto(gasto1);
+anyadirGasto(gasto2);
+anyadirGasto(gasto3);
+anyadirGasto(gasto4);
+anyadirGasto(gasto5);
+anyadirGasto(gasto6);
