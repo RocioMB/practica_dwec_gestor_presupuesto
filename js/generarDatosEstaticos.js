beb2f5dd7c9ef6f4b89d79369840d2ef99a6cc1a
Rocio Martinez Baltanas <mbrocio@gmail.com>
2024-11-11 00:08:17 +0100

    Actualizar y mostrar presupuesto en div

--- js/generarDatosEstaticos.js
+++ js/generarDatosEstaticos.js
@@ -1,3 +1,7 @@
 import {mostrarDatoEnId, mostrarGastoWeb, mostrarGastosAgrupadosWeb} from './gestionPresupuestoWeb.js';
 
 import {mostrarPresupuesto, actualizarPresupuesto, CrearGasto, listarGastos, anyadirGasto, borrarGasto, calcularTotalGastos, calcularBalance, filtrarGastos, agruparGastos} from './gestionPresupuesto.js';
+
+actualizarPresupuesto(1500);
+
+mostrarDatoEnId("presupuesto", mostrarPresupuesto());
--- js/gestionPresupuestoWeb.js
+++ js/gestionPresupuestoWeb.js
@@ -11,24 +11,21 @@ function mostrarGastoWeb(idElemento, gasto) {
     for (let etiqueta of gasto.etiquetas) {
         etiquetasGasto += '<span class="gasto-etiquetas-etiqueta">' + etiqueta + '</span>';
     }
-    elemento.innerHTML = '
-        < div class="gasto">
-         <div class="gasto-descripcion">' + gasto.descripcion + '</div>
-         <div class="gasto-fecha">' + fechaGasto + '</div> 
-         <div class="gasto-valor">' + gasto.valor + '</div> 
-         <div class="gasto-etiquetas">' + etiquetasGasto + '</div> 
-        </div>';
+    elemento.innerHTML = '<div class="gasto">' + 
+         '<div class="gasto-descripcion">' + gasto.descripcion + '</div>' +
+         '<div class="gasto-fecha">' + fechaGasto + '</div>' + 
+         '<div class="gasto-valor">' + gasto.valor + '</div>' +
+         '<div class="gasto-etiquetas">' + etiquetasGasto + '</div></div>';
 }
 
 function mostrarGastosAgrupadosWeb(idElemento, agrup, periodo) {
     let elemento = document.getElementById(idElemento);
     let divAgrupacionDato = "";
     for(const [clave, valor] of Object.entries(agrup)) {
-        divAgrupacionDato += '
-            <div class="agrupacion-dato">
-                <span class="agrupacion-dato-clave">' + clave + '</span>
-                <span class="agrupacion-dato-valor">' + valor + '</span>
-            </div>';
+        divAgrupacionDato += '<div class="agrupacion-dato">' +
+                '<span class="agrupacion-dato-clave">' + clave + '</span>' +
+                '<span class="agrupacion-dato-valor">' + valor + '</span>' +
+            '</div>';
     }
     elemento.innerHTML = '<div class="agrupacion"><h1>Gastos agrupados por ' + periodo + '</h1>' + divAgrupacionDato + '</div >';
 }
