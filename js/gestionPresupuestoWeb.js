cf8fee08742363c64d2b1223134956251dd973e3
Rocio Martinez Baltanas <mbrocio@gmail.com>
2024-11-10 01:16:55 +0100

    Funcion mostrarGastoWeb

--- js/gestionPresupuestoWeb.js
+++ js/gestionPresupuestoWeb.js
@@ -4,3 +4,19 @@ function mostrarDatoEnId(idElemento, valor) {
     console.log(valor);
 }
 
+function mostrarGastoWeb(idElemento, gasto) {
+    let elemento = document.getElementById(idElemento);
+    let fechaGasto = new Date(gasto.fecha).toLocaleString();
+    let etiquetasGasto = "";
+    for (let etiqueta of gasto.etiquetas) {
+        etiquetasGasto += '<span class="gasto-etiquetas-etiqueta">' + etiqueta + '</span>';
+    }
+    elemento.innerHTML = '
+        < div class="gasto">
+         <div class="gasto-descripcion">' + gasto.descripcion + '</div>
+         <div class="gasto-fecha">' + fechaGasto + '</div> 
+         <div class="gasto-valor">' + gasto.valor + '</div> 
+         <div class="gasto-etiquetas">' + etiquetasGasto + '</div> 
+        </div>';
+}
+
