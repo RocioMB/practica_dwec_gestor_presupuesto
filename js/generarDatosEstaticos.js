93a28cfc4eaa58e761258a0822d1d54b1ff8e2f2
Rocio Martinez Baltanas <mbrocio@gmail.com>
2024-11-11 00:31:56 +0100

    Div gastos totales y balance total

--- js/generarDatosEstaticos.js
+++ js/generarDatosEstaticos.js
@@ -6,12 +6,12 @@ actualizarPresupuesto(1500);
 
 mostrarDatoEnId("presupuesto", mostrarPresupuesto());
 
-let gasto1 = new CrearGasto("Compra carne", "23.44", "2021-10-06", "casa", "comida");
-let gasto2 = new CrearGasto("Compra fruta y verdura", "14.25", "2021-09-06", "supermercado", "comida");
-let gasto3 = new CrearGasto("Bonobús", "18.60", "2020-05-26", "transporte");
-let gasto4 = new CrearGasto("Gasolina", "60.42", "2021-10-08", "transporte", "gasolina");
-let gasto5 = new CrearGasto("Seguro hogar", "206.45", "2021-09-26", "casa", "seguros");
-let gasto6 = new CrearGasto("Seguro coche", "195.78", "2021-10-06", "transporte", "seguros");
+let gasto1 = new CrearGasto("Compra carne", 23.44, "2021-10-06", "casa", "comida");
+let gasto2 = new CrearGasto("Compra fruta y verdura", 14.25, "2021-09-06", "supermercado", "comida");
+let gasto3 = new CrearGasto("Bonobús", 18.60, "2020-05-26", "transporte");
+let gasto4 = new CrearGasto("Gasolina", 60.42, "2021-10-08", "transporte", "gasolina");
+let gasto5 = new CrearGasto("Seguro hogar", 206.45, "2021-09-26", "casa", "seguros");
+let gasto6 = new CrearGasto("Seguro coche", 195.78, "2021-10-06", "transporte", "seguros");
 
 anyadirGasto(gasto1);
 anyadirGasto(gasto2);
@@ -19,3 +19,7 @@ anyadirGasto(gasto3);
 anyadirGasto(gasto4);
 anyadirGasto(gasto5);
 anyadirGasto(gasto6);
+
+mostrarDatoEnId("gastos-totales", calcularTotalGastos());
+
+mostrarDatoEnId("balance-total", calcularBalance());
